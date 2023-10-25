6410e6d5091fcea5858f19f1502a9f5ede882593
MateusOK <mateusribeiro-2004@hotmail.com>
2023-10-25 15:47:30 -0300

    feat: user repository

---
+++ src/main/java/br/com/redbag/api/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package br.com.redbag.api.repository;
+
+import br.com.redbag.api.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+
+}
