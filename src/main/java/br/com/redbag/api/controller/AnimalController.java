371bb48fc931b1f3c577a03ef84d2143da1127ea
MateusOK <mateusribeiro-2004@hotmail.com>
2023-10-25 15:44:09 -0300

    feat: animal controller

---
+++ src/main/java/br/com/redbag/api/controller/AnimalController.java
@@ -0,0 +1,44 @@
+package br.com.redbag.api.controller;
+
+import br.com.redbag.api.dto.request.AnimalRequestDto;
+import br.com.redbag.api.dto.response.AnimalResponseDto;
+import br.com.redbag.api.service.AnimalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class AnimalController {
+
+    private final AnimalService animalService;
+
+    @PostMapping("/animals/{userId}")
+    public ResponseEntity<AnimalResponseDto> saveAnimal(@RequestBody AnimalRequestDto requestDto, @PathVariable String userId, UriComponentsBuilder builder) {
+        var response = animalService.saveAnimal(requestDto, userId);
+        var uri = builder.path("animals/{id}").buildAndExpand(response.id()).toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
+
+
+    @GetMapping("/animals/{userId}")
+    public ResponseEntity<List<AnimalResponseDto>> getUserAnimals(@PathVariable String userId) {
+        var response = animalService.getUserAnimals(userId);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/animals/{userId}/{animalId}")
+    public ResponseEntity<Void> deleteAnimal(@PathVariable String userId, @PathVariable String animalId) {
+        animalService.deleteAnimal(userId, animalId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/animals/{userId}/{animalId}")
+    public ResponseEntity<AnimalResponseDto> updateAnimal(@PathVariable String userId, @PathVariable String animalId, @RequestBody AnimalRequestDto request) {
+        var response = animalService.updateAnimal(userId, animalId, request);
+        return ResponseEntity.ok(response);
+    }
+}
