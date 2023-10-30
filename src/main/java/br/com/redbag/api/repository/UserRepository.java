254808aee6797a9555eb9e35c857f5bcb26c6e0c
MateusOK <mateusribeiro-2004@hotmail.com>
2023-10-30 10:44:46 -0300

    refactor: user repository to return userDetails

--- src/main/java/br/com/redbag/api/repository/UserRepository.java
+++ src/main/java/br/com/redbag/api/repository/UserRepository.java
@@ -2,6 +2,7 @@ package br.com.redbag.api.repository;
 
 import br.com.redbag.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,8 +10,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
-    Optional<User> findByUsernameOrEmail(String username, String email);
-    Optional<User> findByUsername(String username);
+    UserDetails findByUsernameOrEmail(String username, String email);
+    UserDetails findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 
