52a95ca290c71b153e6d5f00813e268c24b6a9eb
MateusOK <mateusribeiro-2004@hotmail.com>
2023-10-30 10:19:05 -0300

    feat: auth controller login and register

---
+++ src/main/java/br/com/redbag/api/controller/AuthController.java
@@ -0,0 +1,34 @@
+package br.com.redbag.api.controller;
+
+import br.com.redbag.api.dto.request.LoginRequestDto;
+import br.com.redbag.api.dto.request.RegisterRequestDto;
+import br.com.redbag.api.security.TokenService;
+import br.com.redbag.api.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping(value = {"/login", "/signin"})
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginDto){
+        var token = authService.login(loginDto);
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping(value = {"/register", "/signup"})
+    public ResponseEntity<String> register(@RequestBody RegisterRequestDto registerDto){
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
