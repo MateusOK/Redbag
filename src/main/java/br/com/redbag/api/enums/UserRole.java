aba34a95c8b96d9723dc958f68b113b5a03c885b
MateusOK <mateusribeiro-2004@hotmail.com>
2023-10-30 10:29:43 -0300

    feat: user role enum

---
+++ src/main/java/br/com/redbag/api/enums/UserRole.java
@@ -0,0 +1,16 @@
+package br.com.redbag.api.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+}
