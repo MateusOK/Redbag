29e59aa4213b0e11d35cc9c176e6e37c6fff2a9f
MateusOK <mateusribeiro-2004@hotmail.com>
2023-11-01 14:54:07 -0300

    refactor: optimize imports

--- src/main/java/br/com/redbag/api/entity/History.java
+++ src/main/java/br/com/redbag/api/entity/History.java
@@ -1,7 +1,14 @@
 package br.com.redbag.api.entity;
 
 import br.com.redbag.api.enums.HealthStatus;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- src/main/java/br/com/redbag/api/service/impl/AnimalServiceImpl.java
+++ src/main/java/br/com/redbag/api/service/impl/AnimalServiceImpl.java
@@ -17,7 +17,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
