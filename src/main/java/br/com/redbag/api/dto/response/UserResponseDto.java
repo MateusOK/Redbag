94d7fbc845b4603783b40b1f771a9eba8570ece1
MateusOK <mateusribeiro-2004@hotmail.com>
2023-11-09 20:38:00 -0300

    feat: user response dto

---
+++ src/main/java/br/com/redbag/api/dto/response/UserResponseDto.java
@@ -0,0 +1,15 @@
+package br.com.redbag.api.dto.response;
+
+import br.com.redbag.api.entity.User;
+import br.com.redbag.api.enums.UserRole;
+
+public record UserResponseDto(
+        Long id,
+        String name,
+        String username,
+        String email
+) {
+    public UserResponseDto(User response) {
+        this(response.getId(), response.getName(), response.getUsername(), response.getEmail());
+    }
+}
