79546884b7a5f65c32e3ead7e6bb8a0451c4d1a3
MateusOK <mateusribeiro-2004@hotmail.com>
2023-11-09 20:53:40 -0300

    feat: swagger annotations

--- src/main/java/br/com/redbag/api/controller/UserController.java
+++ src/main/java/br/com/redbag/api/controller/UserController.java
@@ -3,6 +3,9 @@ package br.com.redbag.api.controller;
 import br.com.redbag.api.dto.request.UserRequestDto;
 import br.com.redbag.api.dto.response.UserResponseDto;
 import br.com.redbag.api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -10,21 +13,27 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/api/users", produces = {"application/json"})
+@Tag(name = "Users")
+@SecurityRequirement(name = "bearerAuth")
 public class UserController {
 
     private final UserService userService;
 
-    @DeleteMapping("/api/users/{userId}")
+    @Operation(summary = "Delete a user by id", method = "DELETE")
+    @DeleteMapping("/{userId}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long userId, Authentication authentication) {
         userService.deleteUser(userId, authentication);
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/api/users/{userId}")
+    @Operation(summary = "Update a user by id", method = "PUT")
+    @PutMapping(value = "/{userId}", consumes = {"application/json"})
     public ResponseEntity<UserResponseDto> updateUser(@RequestBody UserRequestDto request, @PathVariable Long userId, Authentication authentication) {
         var response = userService.updateUser(request, userId, authentication);
         return ResponseEntity.ok(response);
