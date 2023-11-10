1e6dc3e50106395a3cef4e128b2df3dd0a72f087
MateusOK <mateusribeiro-2004@hotmail.com>
2023-11-09 21:00:47 -0300

    feat: spring validation

--- src/main/java/br/com/redbag/api/controller/UserController.java
+++ src/main/java/br/com/redbag/api/controller/UserController.java
@@ -6,9 +6,12 @@ import br.com.redbag.api.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 @RequestMapping(value = "/api/users", produces = {"application/json"})
 @Tag(name = "Users")
 @SecurityRequirement(name = "bearerAuth")
@@ -27,14 +31,14 @@ public class UserController {
 
     @Operation(summary = "Delete a user by id", method = "DELETE")
     @DeleteMapping("/{userId}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long userId, Authentication authentication) {
+    public ResponseEntity<Void> deleteUser(@PathVariable @Positive(message = "User id must be higher than 0") Long userId, Authentication authentication) {
         userService.deleteUser(userId, authentication);
         return ResponseEntity.noContent().build();
     }
 
     @Operation(summary = "Update a user by id", method = "PUT")
     @PutMapping(value = "/{userId}", consumes = {"application/json"})
-    public ResponseEntity<UserResponseDto> updateUser(@RequestBody UserRequestDto request, @PathVariable Long userId, Authentication authentication) {
+    public ResponseEntity<UserResponseDto> updateUser(@RequestBody @Valid UserRequestDto request, @PathVariable @Positive(message = "User id must be higher than 0") Long userId, Authentication authentication) {
         var response = userService.updateUser(request, userId, authentication);
         return ResponseEntity.ok(response);
     }
