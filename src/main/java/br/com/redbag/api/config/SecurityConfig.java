6acfbbe9697f64190ad45d1396a8f4761a630900
MateusOK <mateusribeiro-2004@hotmail.com>
2024-11-18 20:31:08 -0300

    feat: enable cors config

--- src/main/java/br/com/redbag/api/config/SecurityConfig.java
+++ src/main/java/br/com/redbag/api/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -40,6 +41,7 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
                         .anyRequest().authenticated()
                 )
+                .cors(Customizer.withDefaults())
                 .addFilterBefore(JwtValidationFilter, UsernamePasswordAuthenticationFilter.class);
         httpSecurity.headers().frameOptions().disable();
         return httpSecurity.build();
